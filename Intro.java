eb1e084ce6e943bf2e6d1f6e1b5b2ab2c0dac072
AkshatRana <akshat.xworkz@gmail.com>
2021-12-20 18:03:21 +0530

    file is added

---
+++ Intro.java
@@ -0,0 +1,13 @@
+class Intro{
+	public static void main(String []args){
+	String[] selfintro = {"Hello Good Afternoon maam/sir" ," First of all I would like to thank you giving me such immense opporunity to introduce myself", "My name is AKshat Rana, From jharkhand", "I am currently pursuing Full Stack Java Developemnt training at Xwrokz Banglore", "Coming to my technical skill i am fimilar with Java, web technology","Coming to my education I completed my Graduation in stream of Mechanical and i also have done my diploma in Mechanical", "My hobbies are like to Play videos games, mobile games and sometimes i play cricket"};
+	System.out.println(selfintro[0]); 
+	System.out.println(selfintro[1]);
+	System.out.println(selfintro[2]);
+	System.out.println(selfintro[3]);
+	System.out.println(selfintro[4]);
+	System.out.println(selfintro[5]);
+	System.out.println(selfintro[6]);
+
+}
+}
